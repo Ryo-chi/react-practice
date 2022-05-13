40a03bf09675d4e0f6dc48347ffcc99be1951ae5
shigetaryo <shigetaryo@outlook.com>
2022-05-13 13:24:01 +0900

    ⚗️ クリックイベントを作りました。いつか消します

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,23 @@
 import Head from 'next/head'
+import { useCallback } from 'react'
 import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
 import { Main } from '../components/Main'
 import styles from '../styles/Home.module.css'
 
 
+
 export default function Home() {
+  
+  const foo = 1;
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
   return (
+
+
     <div className={styles.container}>
       <Head>
         <title>index page</title>
@@ -14,6 +25,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <a href="/about"onClick={handleClick}>
+       ボタン
+      </a>
+     
       <Main page="index"/>
       <Footer />
     </div>
